e32ddc0776022cf03ea6e7a2844d10044e72bcfe
Charles Leifer <coleifer@gmail.com>
2015-08-14 00:19:52 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ lsm_extension = Extension(
 
 setup(
     name='lsm-db',
-    version='0.1.2',
+    version='0.1.3',
     description='Python bindings for the SQLite4 LSM database.',
     author='Charles Leifer',
     author_email='',
