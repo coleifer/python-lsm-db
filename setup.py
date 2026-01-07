c375670648386485e42540f251fff03767e9808b
Charles Leifer <coleifer@gmail.com>
2026-01-06 20:44:32 -0600

    0.7.3
    New build system

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 import glob
 import os
-
 from setuptools import setup
 from setuptools.extension import Extension
 try:
     from Cython.Build import cythonize
-except ImportError:
-    import warnings
-    cython_installed = False
-    warnings.warn('Cython not installed, using pre-generated C source file.')
-else:
     cython_installed = True
-
+except ImportError:
+    cython_installed = False
 
 if cython_installed:
     python_source = 'lsm.pyx'
@@ -24,11 +19,4 @@ lsm_extension = Extension(
     'lsm',
     sources=[python_source] + library_source)
 
-setup(
-    name='lsm-db',
-    version='0.7.2',
-    description='Python bindings for the SQLite4 LSM database.',
-    author='Charles Leifer',
-    author_email='',
-    ext_modules=cythonize([lsm_extension]),
-)
+setup(name='lsm-db', ext_modules=cythonize([lsm_extension]))
